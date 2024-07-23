9f2c4b6f724213e4acdd13d1d9707e9e2a89a64d
Jordan <>
2024-07-23 15:56:43 +0100

    added basic models for listings and users, added to context

---
+++ car-raffle-model/Listing.cs
@@ -0,0 +1,16 @@
+﻿namespace car_raffle_model;
+
+public class Listing
+{
+    public Guid Id { get; set; }
+    public virtual Car Car { get; set; }
+    public Guid CarId { get; set; }
+    public virtual User User { get; set; }
+    public int MaxTicketCount { get; set; }
+    public int CurrentTicketCount { get; set; }
+    public virtual List<Ticket> Tickets { get; set; }
+    public int TicketPrice { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    public DateTime EndDate { get; set; }
+}
